6bdfb7358dd13c22af3a5badd280e1f4e6b562d0
George Vilca <george.vilca29@gmail.com>
2026-04-03 08:48:20 -0500

    Modificacion app/db.py LOCAL Y RENDER

--- app/db.py
+++ app/db.py
@@ -8,14 +8,21 @@ load_dotenv()
 def get_connection():
     database_url = os.getenv("DATABASE_URL")
     
-    if not database_url:
-        raise Exception("DATABASE_URL no esta configurada")
-
+    
+    #Si existe Render
     if database_url and database_url.startswith("postgres://"):
         database_url = database_url.replace("postgres://", "postgresql://", 1)
 
+        return psycopg2.connect(
+            database_url,
+            cursor_factory = RealDictCursor
+        )
+    
+    #Si no existe local
     return psycopg2.connect(
-        database_url,
-        cursor_factory = RealDictCursor
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        cursor_factory=RealDictCursor
     )
-
